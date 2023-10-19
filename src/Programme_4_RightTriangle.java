0733056b0191f6c55fca4f11ecb9a25ecf447d70
vijaychina <vijaykumar.china5@gmail.com>
2023-10-19 15:42:14 +0100

    java-week-1-writting-hw-vijaykumarchina

---
+++ src/Programme_4_RightTriangle.java
@@ -0,0 +1,19 @@
+/**
+ * 4. Write a Java Program to print below triangle
+ *
+ **
+ ***
+ ****
+ *****
+ ******
+ */
+public class Programme_4_RightTriangle {
+    public static void main(String[] args) {
+        System.out.println("*");
+        System.out.println("**");
+        System.out.println("***");
+        System.out.println("****");
+        System.out.println("*****");
+        System.out.println("******");
+    }
+}
